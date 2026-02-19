2f7f5947841bc1860e5cec08aa6e5251ca2c3911
roiLeo <medina.leo42@gmail.com>
2026-02-19 16:52:30 +0100

    ✨ OG image

--- app/app.vue
+++ app/app.vue
@@ -76,8 +76,8 @@ useSeoMeta({
   description,
   ogTitle: title,
   ogDescription: description,
-  ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
-  twitterImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
+  ogImage: '/img/Open-Convert.png',
+  twitterImage: '/img/Open-Convert.png',
   twitterCard: 'summary_large_image'
 })
 
